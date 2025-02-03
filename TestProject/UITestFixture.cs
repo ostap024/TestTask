ee10438d459315005c9c5012db329fb0394d64df
ostapvronskyi <ostapvronskyi@n-ix.com>
2025-02-03 11:52:22 +0200

    fix pipeline

--- TestProject/UITestFixture.cs
+++ TestProject/UITestFixture.cs
@@ -10,7 +10,7 @@ namespace TestProject
             AtataContext.Configure()
                 .UseChrome()
                     .WithArguments("start-maximized")
-                    .WithArguments("headless")
+//.WithArguments("headless")
                 .Build();
         }
 
